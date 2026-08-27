2584e2294f3d15e2df1032dbb72f86cda20525f7
nvros86 <nvros86@users.noreply.github.com>
2026-08-27 22:52:15 +0500

    fix: add missing TabsViewModel import in TabsScreen

--- feature/tabs/src/main/java/com/nexusai/feature/tabs/ui/TabsScreen.kt
+++ feature/tabs/src/main/java/com/nexusai/feature/tabs/ui/TabsScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.nexusai.feature.tabs.viewmodel.TabsViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
